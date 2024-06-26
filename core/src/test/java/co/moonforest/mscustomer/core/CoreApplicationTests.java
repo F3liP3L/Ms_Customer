8b722c3bd196f9fdb62e802c543df5aaec60c15b
Juan Cardona <felipe7u7432@gmail.com>
2024-06-25 20:15:23 -0500

    :heavy_plus_sign: feat: Add project dependencies

---
+++ core/src/test/java/co/moonforest/mscustomer/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.moonforest.mscustomer.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ infrastructure/src/test/java/co/moonforest/mscustomer/infrastructure/InfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package co.moonforest.mscustomer.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
