dd2848fcc0a59fcde3d0fa99c088f98a776fbc36
Juan Felipe Cardona <juan.cardona1338@uco.net.co>
2024-06-26 14:19:26 -0600

    :wrench: add: Add project name and port

--- infrastructure/src/main/java/co/moonforest/mscustomer/infrastructure/MSCustomerApplication.java
+++ infrastructure/src/main/java/co/moonforest/mscustomer/infrastructure/MSCustomerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InfrastructureApplication {
+public class MSCustomerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InfrastructureApplication.class, args);
+        SpringApplication.run(MSCustomerApplication.class, args);
     }
 
 }
