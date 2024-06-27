9b14367224ef8746c1a4e92acfead1947055dff9
Juan Cardona <felipe7u7432@gmail.com>
2024-06-27 18:36:44 -0500

    :sparkles: feat: Add customer aggregate structure

---
+++ core/src/main/java/co/moonforest/mscustomer/core/domain/aggregate/AggregateRoot.java
@@ -0,0 +1,6 @@
+package co.moonforest.mscustomer.core.domain.aggregate;
+
+import co.moonforest.mscustomer.core.domain.Entity;
+
+public abstract class AggregateRoot<E extends Entity<ID>, ID> {
+}
---
+++ core/src/main/java/co/moonforest/mscustomer/core/domain/aggregate/customer/CustomerEntity.java
@@ -0,0 +1,8 @@
+package co.moonforest.mscustomer.core.domain.aggregate.customer;
+
+import co.moonforest.mscustomer.core.domain.Entity;
+
+import java.util.UUID;
+
+public class CustomerEntity extends Entity<UUID> {
+}
