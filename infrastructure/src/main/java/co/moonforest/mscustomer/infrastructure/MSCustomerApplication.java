6bf6fea9f973e0c7b2258ab5a33181ea51efbe3d
Juan Felipe Cardona <juan.cardona1338@uco.net.co>
2024-07-24 11:10:16 -0600

    :heavy_plus_sign:  add: Add spring starter web dependency

--- infrastructure/src/main/java/co/moonforest/mscustomer/infrastructure/MSCustomerApplication.java
+++ infrastructure/src/main/java/co/moonforest/mscustomer/infrastructure/MSCustomerApplication.java
@@ -2,8 +2,12 @@ package co.moonforest.mscustomer.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"co.moonforest.mscustomer"})
+@ComponentScan(basePackages = {"co.moonforest.mscustomer"})
 public class MSCustomerApplication {
 
     public static void main(String[] args) {
